3f06463cb86abe57c7d8ce9595c030eed6d21d8c
danfilms9 <danfilms9@gmail.com>
2026-01-23 21:51:38 -0500

    Add /epk page with Spline embed and update Oceanic branding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { IphoneShell } from './components/Iphone/IphoneShell'
+import { EPKPage } from './components/EPKPage'
 
 function App() {
-  return <IphoneShell />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/epk" element={<EPKPage />} />
+        <Route path="*" element={<IphoneShell />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/components/EPKPage.tsx
@@ -0,0 +1,15 @@
+import './EPKPage.css'
+
+export function EPKPage() {
+  return (
+    <div className="epk-container">
+      <iframe
+        src="https://my.spline.design/oceanicepk-cf85e29646ce94f653e75d4c042997bd/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        title="EPK Spline"
+      />
+    </div>
+  )
+}
